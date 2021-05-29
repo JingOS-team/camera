70fb55d304ecc78b6e33eb5bd9aeba1a738ebb9d
hegang <yzshang@xlauncher.cn>
2021-05-29 14:21:17 +0800

    clear project

--- Messages.sh
+++
@@ -1,3 +0,0 @@
-# call xgettext on all source files. If your sources have other filename
-# extensions besides .cc, .cpp, and .h, just add them in the find call.
-$XGETTEXT $(find . -name \*.cc -o -name \*.cpp -o -name \*.h -o -name \*.qml) -o $podir/plasma-camera.pot
